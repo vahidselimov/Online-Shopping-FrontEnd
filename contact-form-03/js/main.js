98fc70d9b023071eff6a646a1923952d156cb68b
vahidselimov <100331397+vahidselimov@users.noreply.github.com>
2022-12-31 14:10:44 +0400

    Online Shopping Corrections Part2

--- contact-form-03/js/main.js
+++ contact-form-03/js/main.js
@@ -32,9 +32,9 @@
 				// 		waitText = 'Submitting...';
 
 				// 	$.ajax({   	
-				//       type: "POST",
-				//       url: "php/sendEmail.php",
-				//       data: $(form).serialize(),
+				    //   type: "POST",
+				    //   url: "php/sendEmail.php",
+				    //   data: $(form).serialize(),
 
 				//       beforeSend: function() { 
 				//       	$submit.css('display', 'block').text(waitText);
@@ -83,3 +83,27 @@
 	contactForm();
 
 })(jQuery);
+$(document).on('click','ul li',function(){
+	$(this).addClass('active').siblings().removeClass('active')
+
+  })
+  let allMenuDiv = document.querySelectorAll("#tab-menu .menu li");
+let allItemElem = document.querySelectorAll("#tab-menu .body .item");
+for (const menu of allMenuDiv) {
+	menu.addEventListener("click", function () {
+	  let active = document.querySelector(".active")
+	  active.classList.remove("active")
+	  this.classList.add("active");
+  
+	  let id = this.getAttribute("data-id")
+	  for (const item of allItemElem) {
+		if (item.getAttribute("data-id") == id) {
+		  item.classList.remove("d-none")
+		}
+		else {
+		  item.classList.add("d-none")
+		}
+	  }
+	})
+  }
+  
