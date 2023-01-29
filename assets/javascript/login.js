6e52546ff2845abedb1d71a30fb4cafa25f0cbde
vahidselimov <100331397+vahidselimov@users.noreply.github.com>
2023-01-29 20:38:52 +0400

    Register entries

--- assets/javascript/login.js
+++ assets/javascript/login.js
@@ -14,7 +14,33 @@ $(".toggle-password").click(function () {
         return false;
       });
   });
+
+  var btn=document.getElementById("btn")
+  var modal=document.getElementById("myModal")
+  var span = document.getElementsByClassName("close")[0];
   
+  //Modals Register//
+ 
+    btn.onclick = function() {
+      modal.style.display = "block";
+    }
+    
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+      modal.style.display = "none";
+    }
+    
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+      if (event.target == modal) {
+        modal.style.display = "none";
+      }
+    }
+  
+
+
+  //Bu istifade olunmur numune kimi yazilib
+  //---------------------------------//
   // function showAlert() {
   //   var myText = "Adminin Gmail Addressi: Vahidsalimov12@gmail.com"
   //  var myText2="SuperAdminin Gmail Addressi: tu6g17bsz@gmail.com";
